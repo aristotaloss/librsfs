5955b22e70370bc7994a270fa5b412513f24844e
Bart Pelle <blackinmen@gmail.com>
2015-04-22 10:38:21 +0200

    Make code less verbose

--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -20,19 +20,22 @@ FSResult FileSystem::loadDirectory(char *directory) {
 
     // Discover main_file_cache.dat2 (the '/' operator glues two paths)
     path mainFile = path(directory) / string("main_file_cache.dat2");
-    if (!filesystem::exists(mainFile)) {
-        return FileSystem::E_NO_MAINFILE;
+    if (!exists(mainFile)) {
+        return E_NO_MAINFILE;
     }
 
     // Load all the *.idx files
     map<int, path*> validIndices; // Map to hold our .idx file paths
     for (directory_entry e : make_iterator_range(directory_iterator(directory))) { // Loop
-        if (starts_with(e.path().extension().string(), ".idx") && e.path().string().size() > 4) { // Is this an index descriptor?
-            int idxId = atoi(e.path().extension().string().substr(4).c_str()); // Substring from 4, parse as integer.
+        path current = e.path();
+        string extension = current.extension().string();
+
+        if (starts_with(extension, ".idx") && extension.size() > 4) { // Is this an index descriptor?
+            int idxId = atoi(extension.substr(4).c_str()); // Substring from 4, parse as integer.
 
             // Check if the range is valid. If so, store it in our map.
             if (idxId >= 0 && idxId <= 255) {
-                validIndices[idxId] = new path(e); // Copy, otherwise it falls out of scope
+                validIndices[idxId] = new path(current); // Copy, otherwise it falls out of scope
             }
         }
     }
