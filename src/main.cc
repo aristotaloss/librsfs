4d24d6f6889b28458a12f1cfb8c007f60328c0eb
Bart Pelle <blackinmen@gmail.com>
2016-02-05 23:42:37 +0100

    Don't use boost for fs

--- src/main.cc
+++ src/main.cc
@@ -7,19 +7,20 @@ using namespace std;
 int main() {
     FileSystem filesystem;
     try {
-        filesystem.LoadDirectory(const_cast<char *>("/home/bart/eocache/data 851/"));
+        filesystem.LoadDirectory(const_cast<char *>("C:\\Users\\Bart\\Documents\\osrs-server\\data\\nigga"));
     } catch (const std::exception &exc) {
         printf("Error loading directory: %s\n", exc.what());
         return 0;
     }
 
-    //for (auto a = 0; a < filesystem.GetIndexCount(); a++) {
-    int a = 255;
-        printf("Index %d has %d folders\n", a, filesystem.GetIndex(a).GetEntryCount());
+    for (auto a = 0; a < filesystem.GetIndexCount(); a++) {
+    //int a = 255;
+        printf("Index %d has %d folders\n", a, filesystem.GetIndex(a)->GetEntryCount());
 
-        for (int f = 0; f < filesystem.GetIndex(a).GetEntryCount(); f++) {
+        for (int f = 0; f < filesystem.GetIndex(a)->GetEntryCount(); f++) {
             try {
-                FolderInfo info = filesystem.GetIndex(a).GetFolderInfo(f);
+                FolderInfo info = filesystem.GetIndex(a)->GetFolderInfo(f);
+				printf("%d,%d,%d\n", info.GetId(), info.GetOffset(), info.GetSize());
                 vector<char> data0_11;
                 int rdnum = filesystem.Read(info, data0_11);
 
@@ -38,10 +39,10 @@ int main() {
             	printf("error with %d: %s\n", f, exc.what());
             }
         }
-    //}
+    }
 
     printf("Number of indices: %d\n", filesystem.GetIndexCount());
     printf("Has index 15: %d, has 16: %d\n", filesystem.HasIndex(15), filesystem.HasIndex(16));
-    printf("Number of files in model directory: %d\n", filesystem.GetIndex(7).GetEntryCount());
+    printf("Number of files in model directory: %d\n", filesystem.GetIndex(7)->GetEntryCount());
     return 0;
 }
