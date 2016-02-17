d13f7988b4543af9de1805a7a8549528afc95544
Bart Pelle <blackinmen@gmail.com>
2016-02-17 21:24:55 +0100

    Add dump command to command line tool

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,15 @@
 #include <fstream>
 #include "file_system.h"
 #include "compression.h"
+#include <stdio.h>
+
+#ifdef WINDOWS
+#include <direct.h>
+#define xpf_cwd _getcwd
+#else
+#include <unistd.h>
+#define xpf_cwd getcwd
+#endif
 
 using namespace std;
 
@@ -11,23 +20,29 @@ void process_lines(istream &);
 void help() {
 	cout << endl;
 	cout << "load <path>" << endl;
-	cout << "| Loads a file store from the specified path." << endl;
-	cout << "| > load /home/me/filestore" << endl;
+	cout << "   Loads a file store from the specified path." << endl;
+	cout << "   e.g.: load /home/me/filestore" << endl;
 	cout << endl;
 
 	cout << "unload" << endl;
-	cout << "| Closes and unloads the currently opened filestore." << endl;
+	cout << "   Closes and unloads the currently opened filestore." << endl;
 	cout << endl;
 
 	cout << "indices" << endl;
-	cout << "| Shows a summary of the indices present and their number of (estimated) folders." << endl;
+	cout << "   Shows a summary of the indices present and their number of (estimated) folders." << endl;
 	cout << endl;
 
 	cout << "info <index> [folder]" << endl;
-	cout << "| Displays low-level information about an index and/or folder." << endl;
-	cout << "| The index is required, folder is optional." << endl;
-	cout << "| If the folder is not provided, all folders will be printed." << endl;
-	cout << "| > info 2 16" << endl;
+	cout << "   Displays low-level information about an index and/or folder." << endl;
+	cout << "   The index is required, folder is optional." << endl;
+	cout << "   If the folder is not provided, all folders will be printed." << endl;
+	cout << "   e.g.: info 2 16" << endl;
+	cout << endl;
+
+	cout << "dump <index> <folder>" << endl;
+	cout << "   Writes the contents of a given folder in an index to a file." << endl;
+	cout << "   This file will be called dump_<index>_<folder>.dat." << endl;
+	cout << "   e.g.: dump 7 18471" << endl;
 }
 
 void load(string line) {
@@ -103,7 +118,52 @@ void execute_macro(string line) {
 }
 
 void info(string line) {
-	
+	try {
+		string index = line.substr(5, line.find(' '));
+		cout << "Index: " << index << endl;
+	} catch (exception &exc) {
+		cerr << "Error dumping folder." << endl;
+		cerr << exc.what() << endl;
+	}
+}
+
+void dump(string line) {
+	if (filesystem == nullptr) {
+		cerr << "Cannot list indices without a loaded filestore. See 'help'." << endl;
+		return;
+	}
+
+	try {
+		string params = line.substr(5);
+		string index = params.substr(0, params.find(' '));
+		string folder = params.substr(params.find(' ') + 1);
+
+		int index_int = atoi(index.c_str());
+		int folder_int = atoi(folder.c_str());
+
+		cout << "Dumping data from " << index_int << ":" << folder_int << " to dump_" << index_int << "_" << folder_int << ".dat.." << endl;
+
+		auto idx = filesystem->get_index(index_int);
+		if (idx == nullptr) {
+			cout << "Index " << idx << " does not exist." << endl;
+			return;
+		}
+
+		auto info = idx->get_folder_info(folder_int);
+		if (info.get_size() < 1) {
+			cout << "This folder does not exist, or has no data." << endl;
+			return;
+		}
+
+		vector<char> data;
+		int totalread = idx->get_decompressed(folder_int, data);
+		ofstream output("dump_" + to_string(index_int) + "_" + to_string(folder_int) + ".dat", ios::out | ios::binary);
+		output.write(data.data(), data.size());
+		cout << "Dumped " << totalread << " bytes." << endl;
+	} catch (exception &exc) {
+		cerr << "Error dumping folder." << endl;
+		cerr << exc.what() << endl;
+	}
 }
 
 void process_command(string line) {
@@ -119,6 +179,8 @@ void process_command(string line) {
 		list_indices();
 	} else if (command == "macro") {
 		execute_macro(line);
+	} else if (command == "dump") {
+		dump(line);
 	} else {
 		cout << "Unknown command. Type 'help' for options." << endl;
 	}
@@ -135,6 +197,11 @@ void process_lines(istream &stream) {
 }
 
 int main() {
+	// Print current working dir..
+	char cwd[512];
+	xpf_cwd(cwd, 512);
+	cout << "Working directory: " << cwd << endl << endl;
+
 	process_lines(cin);
 	return 0;
 }
